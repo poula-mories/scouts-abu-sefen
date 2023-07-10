f28c20a5f9e0457391e47edc5e0059320de631e6
Poula Mories <poulamories5@gmail.com>
2023-07-10 14:46:20 +0300

    Update databaseConnectionSequelize.ts

--- src/databaseConnectionSequelize.ts
+++ src/databaseConnectionSequelize.ts
@@ -17,7 +17,7 @@ const {
 //         dialect: 'postgres',
 //        logging: false
 //       });
-const sequelize = new Sequelize(process.env.DB_LINK, {
+const sequelize = new Sequelize('postgres://scouts_abu_sefen_db_user:LDefvKBL5PIRNcT10H2tHWSOYRhm5FJL@dpg-ciltqmunqqlfm4dcvkb0-a/scouts_abu_sefen_db', {
   dialect: 'postgres',
   protocol: 'postgres',
   dialectOptions: {}, //removed ssl
