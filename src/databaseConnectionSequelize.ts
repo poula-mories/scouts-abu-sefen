4987ca98bf3afe424dc187ba48a27d9f0dac69b4
Poula Mories <poulamories5@gmail.com>
2023-07-10 14:36:32 +0300

    Update databaseConnectionSequelize.ts

--- src/databaseConnectionSequelize.ts
+++ src/databaseConnectionSequelize.ts
@@ -12,10 +12,16 @@ const {
 } = process.env
 
 
-const sequelize = new Sequelize(POSTGRES_DB as string, POSTGRES_USER as string , POSTGRES_PASSWORD as string , {
-        host: POSTGRES_HOST,
-        dialect: 'postgres',
-        logging: false
-      });
+// const sequelize = new Sequelize(POSTGRES_DB as string, POSTGRES_USER as string , POSTGRES_PASSWORD as string , {
+//         host: POSTGRES_HOST,
+//         dialect: 'postgres',
+//        logging: false
+//       });
+const sequelize = new Sequelize(process.env.DB_LINK, {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  dialectOptions: {}, //removed ssl
+  logging: false
+});
 
 export default sequelize
